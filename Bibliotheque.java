b340572470734455656a68677b4fe7a7330f4d35
soumia <kablaouisoumia75@gmai.com>
2024-12-20 16:27:26 +0100

    creer la fonction ajouter

--- Bibliotheque.java
+++ Bibliotheque.java
@@ -3,3 +3,7 @@ import java.util.List;
 
 public class Bibliotheque {
     private List<Livre> livres = new ArrayList<>();
+    public void ajouter(String titre, String auteur, String isbn, boolean disponibilite) {
+        livres.add(new Livre(titre, auteur, isbn, disponibilite));
+        System.out.println("Le livre est  ajouter ! ");
+    }
