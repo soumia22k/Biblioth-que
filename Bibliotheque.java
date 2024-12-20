566bce9d2392b68512d0eb80ab252378746f5b7c
soumia <kablaouisoumia75@gmai.com>
2024-12-20 16:31:05 +0100

    creer la fonction afficher

--- Bibliotheque.java
+++ Bibliotheque.java
@@ -7,3 +7,21 @@ public class Bibliotheque {
         livres.add(new Livre(titre, auteur, isbn, disponibilite));
         System.out.println("Le livre est  ajouter ! ");
     }
+    public void afficher() {
+        if (livres.isEmpty()) {
+            System.out.println("aucun livre !!!");
+            return;
+        }
+        for (int i = 0; i < livres.size(); i++) {
+            Livre livre = livres.get(i);
+            System.out.println("= la listes des livres =");
+            System.out.println("Titre : " + livre.titre);
+            System.out.println("Auteur : " + livre.auteur);
+            System.out.println("ISBN : " + livre.isbn);
+            if (livre.disponibilite) {
+                System.out.println("le liver est disponible.");
+            }else System.out.println("le liver n'est pas disponible. ");
+        }
+
+    }
+}
